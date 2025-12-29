e721d8c2faee31f6267f92ae16bcd3f174a1e69e
nala-hn <nalahoirunnisa7@gmail.com>
2025-12-29 12:10:08 +0700

    fix: auth API

--- frontend-react-ts/vite.config.ts
+++ frontend-react-ts/vite.config.ts
@@ -6,5 +6,15 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),],
+    tailwindcss(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
