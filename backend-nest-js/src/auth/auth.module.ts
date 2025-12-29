ff3d7857faf484d3897720d853a740e25f67ea26
nala-hn <nalahoirunnisa7@gmail.com>
2025-12-29 11:44:37 +0700

    fix: auth

--- backend-nest-js/src/auth/auth.module.ts
+++ backend-nest-js/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('SECRET_KEY'),
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: parseInt(configService.get<string>('JWT_EXPIRES_IN', '1800'), 10) },
       }),
       inject: [ConfigService],
